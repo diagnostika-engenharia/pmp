0de32a7b1b0d3a6061d6696231cb6b9844a02638
Diagnostika <contato@diagnostikaengenharia.com.br>
2026-05-24 20:42:57 -0300

    fix v28: fmtMD/fmtProx agora globais (eram locais a renderHome e quebravam renderCondoDetail silenciosamente)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diagnostika-v27';
+const CACHE = 'diagnostika-v28';
 const ASSETS = ['./', './index.html', './manifest.json', './assets/logo-dark.png', './assets/logo-vertical.png', './assets/icon.png'];
 
 self.addEventListener('install', e => {
