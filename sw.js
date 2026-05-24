c0832917c9574697ba01653476f3b68a84c3cfeb
Diagnostika <contato@diagnostikaengenharia.com.br>
2026-05-23 23:52:20 -0300

    HOTFIX v24: backticks triplos no prompt quebravam o template literal e travavam toda a JS

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diagnostika-v23';
+const CACHE = 'diagnostika-v24';
 const ASSETS = ['./', './index.html', './manifest.json', './assets/logo-dark.png', './assets/logo-vertical.png', './assets/icon.png'];
 
 self.addEventListener('install', e => {
