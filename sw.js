1f153b3f1eb3134cca8474cca723e13f813658e9
Diagnostika <contato@diagnostikaengenharia.com.br>
2026-05-25 14:21:46 -0300

    fix: cap de itens por visita (MC=6, MM=9) com roll-forward para folga de reformas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diagnostika-v56';
+const CACHE = 'diagnostika-v57';
 const ASSETS = ['./', './index.html', './manifest.json', './assets/logo-dark.png', './assets/logo-vertical.png', './assets/icon.png'];
 
 self.addEventListener('install', e => {
