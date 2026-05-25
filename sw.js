b8b8e71cf4bad3b58a44f7f030c9615b448b7698
Diagnostika <contato@diagnostikaengenharia.com.br>
2026-05-25 09:43:34 -0300

    feat: botao voltar em todas as telas (historico, agenda, plano)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diagnostika-v43';
+const CACHE = 'diagnostika-v44';
 const ASSETS = ['./', './index.html', './manifest.json', './assets/logo-dark.png', './assets/logo-vertical.png', './assets/icon.png'];
 
 self.addEventListener('install', e => {
