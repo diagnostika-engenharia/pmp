42f9e201262fd34be5d3e588ba01319b56d1c217
Diagnostika <contato@diagnostikaengenharia.com.br>
2026-05-25 00:57:41 -0300

    v40: selecao multipla de visitas (home + historico) com checkbox - excluir em lote local+nuvem

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diagnostika-v39';
+const CACHE = 'diagnostika-v40';
 const ASSETS = ['./', './index.html', './manifest.json', './assets/logo-dark.png', './assets/logo-vertical.png', './assets/icon.png'];
 
 self.addEventListener('install', e => {
