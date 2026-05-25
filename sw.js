72efda7000776d9121abbd9a10cef6f3eb7d440e
Diagnostika <contato@diagnostikaengenharia.com.br>
2026-05-25 10:01:35 -0300

    feat: saudacao personalizada conforme usuario logado

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diagnostika-v47';
+const CACHE = 'diagnostika-v48';
 const ASSETS = ['./', './index.html', './manifest.json', './assets/logo-dark.png', './assets/logo-vertical.png', './assets/icon.png'];
 
 self.addEventListener('install', e => {
