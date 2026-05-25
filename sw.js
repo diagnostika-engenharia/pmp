8db4bf48e94afcf461edd04b336a1c7e1dbf542a
Diagnostika <contato@diagnostikaengenharia.com.br>
2026-05-25 09:51:15 -0300

    feat: correcao ortografica pt-BR em todos os campos de texto

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diagnostika-v45';
+const CACHE = 'diagnostika-v46';
 const ASSETS = ['./', './index.html', './manifest.json', './assets/logo-dark.png', './assets/logo-vertical.png', './assets/icon.png'];
 
 self.addEventListener('install', e => {
