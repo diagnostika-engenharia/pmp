c757ff285423098ec68ce3bfef6c2c7b5aa1aaae
Diagnostika <contato@diagnostikaengenharia.com.br>
2026-05-25 14:45:48 -0300

    fix(critico): algoritmo load balancing - distribuicao uniforme 5-7 itens/visita

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diagnostika-v57';
+const CACHE = 'diagnostika-v58';
 const ASSETS = ['./', './index.html', './manifest.json', './assets/logo-dark.png', './assets/logo-vertical.png', './assets/icon.png'];
 
 self.addEventListener('install', e => {
