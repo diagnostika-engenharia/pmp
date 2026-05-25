b9382d2df39f7d90cb10e09d2743b7b9a04ee79b
Diagnostika <contato@diagnostikaengenharia.com.br>
2026-05-24 21:18:56 -0300

    v30: Reforma com campos bloco/apartamento/morador - aparecem no formulario, header do checklist e sao enviados ao Supabase (colunas adicionadas)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diagnostika-v29';
+const CACHE = 'diagnostika-v30';
 const ASSETS = ['./', './index.html', './manifest.json', './assets/logo-dark.png', './assets/logo-vertical.png', './assets/icon.png'];
 
 self.addEventListener('install', e => {
