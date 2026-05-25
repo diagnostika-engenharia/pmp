a5a6eab6e3abb23d00ffdbf795c3396bf66c6194
Diagnostika <contato@diagnostikaengenharia.com.br>
2026-05-25 10:13:17 -0300

    fix(seg): gate estrito de auth - bloqueia render ate validar sindico

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diagnostika-v48';
+const CACHE = 'diagnostika-v49';
 const ASSETS = ['./', './index.html', './manifest.json', './assets/logo-dark.png', './assets/logo-vertical.png', './assets/icon.png'];
 
 self.addEventListener('install', e => {
