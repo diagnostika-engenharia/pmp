9109bec0fdae06d3d43835f3560942f0d305a788
Diagnostika <contato@diagnostikaengenharia.com.br>
2026-05-25 02:00:40 -0300

    v42: remove pagina de planejamento financeiro do PDF do sindico - foco apenas na parte tecnica (responsabilidade financeira eh da sindica)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diagnostika-v41';
+const CACHE = 'diagnostika-v42';
 const ASSETS = ['./', './index.html', './manifest.json', './assets/logo-dark.png', './assets/logo-vertical.png', './assets/icon.png'];
 
 self.addEventListener('install', e => {
