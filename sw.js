241ea09e0ab9feb9c72e94c31dd809424dbfaa6e
Diagnostika <contato@diagnostikaengenharia.com.br>
2026-05-25 09:56:44 -0300

    feat: datas no padrao brasileiro DD/MM/YYYY

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diagnostika-v46';
+const CACHE = 'diagnostika-v47';
 const ASSETS = ['./', './index.html', './manifest.json', './assets/logo-dark.png', './assets/logo-vertical.png', './assets/icon.png'];
 
 self.addEventListener('install', e => {
