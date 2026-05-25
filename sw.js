cdb03ecc5ddba6cfab0c0d079a623db48f7688ba
Diagnostika <contato@diagnostikaengenharia.com.br>
2026-05-24 22:09:05 -0300

    fix v33: PDF reforma com titulo correto, dados de bloco/apto/morador, emblema do condo no cabecalho + corrigir duplicacao na impressao (regra antiga forcava print-area visivel junto com preview)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diagnostika-v32';
+const CACHE = 'diagnostika-v33';
 const ASSETS = ['./', './index.html', './manifest.json', './assets/logo-dark.png', './assets/logo-vertical.png', './assets/icon.png'];
 
 self.addEventListener('install', e => {
