b2aade01a13ccb36cff3d3ccae35d5849849def2
Diagnostika <contato@diagnostikaengenharia.com.br>
2026-05-29 00:03:20 -0300

    chore(sw): v83 -> v84 (textos atualizados em morador.html e portaria)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diagnostika-v83';
+const CACHE = 'diagnostika-v84';
 const ASSETS = ['./', './index.html', './manifest.json', './assets/logo-dark.png', './assets/logo-vertical.png', './assets/icon.png'];
 
 self.addEventListener('install', e => {
